6478389608c631ea8543fcad39e782841b678cbe
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-11 11:23:32 +0530

    Create creds.tf

---
+++ creds.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIA5XPOHFCD5GX7V4QN"
+}
+variable "aws_secret_key" {
+default = "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"
+}
+variable "aws_region" {
+default = "us-east-1"
+}
